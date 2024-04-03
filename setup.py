130b5348b66502786a5e8765d1f95e66fd1aa0c7
Josh Burroughs <josh@qhool.com>
2024-04-03 14:09:34 -0400

    Add dependencies

--- setup.py
+++ setup.py
@@ -7,4 +7,13 @@ setup(
     description = "builds a word cloud of available food trucks in San Francisco, California",
     author = "Josh Burroughs",
     author_email = "josh@qhool.com",
+
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    python_requires='>=3.7, <4',
+    install_requires = [
+        "pandas",
+        "numpy<2",
+        "wordcloud",
+        "geocoder"
+    ]
 )
